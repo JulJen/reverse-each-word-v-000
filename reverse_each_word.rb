d557e50a87667d4707011b8c7375360d2de441b5
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 02:24:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(word)
-  
-  new_arr = word.reverse
+
+  new_arr = word.reverse.sort
   "#{new_arr}"
   # "[#{new_arr}].split.reverse.join (" ")"
   # new_word << "#{new_word}.reverse"
