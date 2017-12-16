18a8021f30baf70be9192ea2493116efa53bbc18
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 02:26:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 
-def reverse_each_word(word)
-
-  new_arr = word.reverse.sort
-  "#{new_arr}"
-  # "[#{new_arr}].split.reverse.join (" ")"
-  # new_word << "#{new_word}.reverse"
-  # my_statement = word.map {|x| x.reverse}
+def reverse_each_word(phrase)
+  reversed = []
+  phrase.split.each do |word|
+    reversed.unshift word
+end
+reversed.join(' ')
+end
 end
