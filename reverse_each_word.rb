163d52ebbbaefa3c502731a583959550d6a38949
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 01:44:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 
 def reverse_each_word(word)
-  my_statement = []
-  word.map {|x| x.reverse}
-    my_statements << "I love #{x} on my burgers!"
+  my_statement = word.map {|x| x.reverse}
   end
-  my_statements
+  my_statement
 end
