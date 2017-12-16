ac3c28ca4a9484e92178c549ea251a7b9b96075c
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 02:31:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,11 @@
 
 def reverse_each_word(phrase)
-  reverse = (phrase.split.collect do |word|
+  # reverse = (phrase.split.collect do |word|
+  #   word.reverse
+  # end).join(' ')
+
+  reverse = (phrase.collect do |word|
     word.reverse
   end).join(' ')
+
 end
