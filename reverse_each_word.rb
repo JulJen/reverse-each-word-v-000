b4065cf5a106b19086a521cfa1022a9e5585e916
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 01:56:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 
 def reverse_each_word(word)
   new_arr = word.split.reverse.join(' ')
-  "[#{new_arr}].split.reverse.join (" ")"
+  "#{new_arr}"
+  # "[#{new_arr}].split.reverse.join (" ")"
   # new_word << "#{new_word}.reverse"
   # my_statement = word.map {|x| x.reverse}
 end
