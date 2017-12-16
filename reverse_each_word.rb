3abd7f8114cd54c1c2e93fb61b726a39187655a3
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-16 02:04:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 
 def reverse_each_word(word)
-  new_arr = word.collect do |x|
-    "#{x}"
-  end
+  # new_arr = word.collect { |x| x
+    # "#{x}"
+  # end
 
 
-  # new_arr = "#{x}".split.reverse.join(' ')
+  new_arr = "#{x}".split.reverse.join(' ')
 
   # "[#{new_arr}].split.reverse.join (" ")"
   # new_word << "#{new_word}.reverse"
